b1699180210d5e424565eecc1db3377e6e914076
mochamadyusuf <mochamadd.yusuf@gmail.com>
2022-07-09 00:04:45 +0700

    tugas 7 javascript niomic

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+let tinggi = [160, 165, 170, 172, 175, 178, 180, 185, 190, 193];
+
+// perulangan biasa
+for (i = 0; i < tinggi.length; i++) {
+  console.log(tinggi[i]);
+}
+
+// perulangan for of
+for (let t of tinggi) {
+  console.log(t);
+}
